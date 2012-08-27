b1bab9aec75a1c0da66cbbfde64e491270d22b22
newuser978 <amoschini+newuser978@makingsense.com>
2012-08-27 13:57:58 -0300

    Edit Home screen message

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to GitHub demo!";
 
             return View();
         }
