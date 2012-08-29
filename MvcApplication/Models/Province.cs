a1255b94a1dcc417c9c4bb29bd9522dc41a71100
newuser978 <amoschini+newuser978@makingsense.com>
2012-08-29 17:14:07 -0300

    Add Province model

---
+++ MvcApplication/Models/Province.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication.Models
+{
+    public class Province
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public string Iso { get; set; }
+        public string Capital { get; set; }
+        public int Area { get; set; }
+        public decimal Density
+        {
+            get { return Population / Area; }
+        }
+    }
+}
