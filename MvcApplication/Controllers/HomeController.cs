323bdd633f631f31b490c572325180f5e1a66262
newuser978 <amoschini+newuser978@makingsense.com>
2012-08-29 17:15:04 -0300

    Add provinces to index view
    Demo province included in index controller

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcApplication.Models;
 
 namespace MvcApplication.Controllers
 {
@@ -11,8 +12,17 @@ namespace MvcApplication.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to GitHub demo! (edited online)";
-
-            return View();
+            var provinces = new[] {
+                new Province() 
+                { 
+                    Name = "Buenos Aires", 
+                    Population = 15625084,
+                    Area = 307571,
+                    Capital = "La Plata",
+                    Iso = "AR-B"
+                }
+            };
+            return View(provinces.OrderBy(x => x.Name));
         }
 
         public ActionResult About()
